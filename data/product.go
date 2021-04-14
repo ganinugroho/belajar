d926e9485242a275b3cf6a641d9379fd03241fea
Gani Pungkas Nugroho <gani.nugroho@live.com>
2021-04-14 13:33:41 +0700

    test validasi

--- data/product.go
+++ data/product.go
@@ -4,15 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 	"time"
+
+	"github.com/go-playground/validator"
 )
 
 type Product struct {
 	ID          int     `json:"id"`
-	Name        string  `json:"name"`
+	Name        string  `json:"name" validate:"required"`
 	Description string  `json:"description"`
-	Price       float32 `json:"price"`
-	SKU         string  `json:"sku"`
+	Price       float32 `json:"price" validate:"gt=0"`
+	SKU         string  `json:"sku" validate:"required,sku"`
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"`
@@ -88,4 +91,19 @@ func findProduct(_id int) (*Product, int, error) {
 func getNextID() int {
 	p := productList[len(productList)-1]
 	return p.ID + 1
+}
+
+func (p *Product) Validate() error {
+	validate := validator.New()
+	validate.RegisterValidation("sku", validateSKU)
+	return validate.Struct(p)
+}
+
+func validateSKU(v validator.FieldLevel) bool {
+	reg := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]+-`)
+	matches := reg.FindAllString(v.Field().String(), -1)
+	if len(matches) != 1 {
+		return false
+	}
+	return true
 }
